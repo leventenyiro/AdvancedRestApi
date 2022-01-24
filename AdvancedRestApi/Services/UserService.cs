b611cae0d3b8a05b3ddc759615c2c88bfc161dbb
Levente Nyiro <lnyiro@berriesand.co>
2022-01-24 12:47:37 +0100

    Add Services

---
+++ AdvancedRestApi/Services/UserService.cs
@@ -0,0 +1,49 @@
+﻿using AdvancedRestApi.Data;
+using AdvancedRestApi.Interfaces;
+using AdvancedRestApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdvancedRestApi.Services
+{
+    public class UserService : IUser
+    {
+        private UserDbContext _dbContext;
+        public UserService(UserDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddUser(User user)
+        {
+            await _dbContext.Users.AddAsync(user);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteUser(Guid id)
+        {
+            var user = await _dbContext.Users.FindAsync(id);
+            _dbContext.Users.Remove(user);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<List<User>> GetAllUsers()
+        {
+            return await _dbContext.Users.ToListAsync();
+        }
+
+        public async Task<User> GetUserById(Guid id)
+        {
+            return await _dbContext.Users.FindAsync(id);
+        }
+
+        public async Task UpdateUser(Guid id, User user)
+        {
+            var userObj = await _dbContext.Users.FindAsync(id);
+            userObj.Name = user.Name;
+            userObj.Address = user.Address;
+            userObj.Phone = user.Phone;
+            userObj.BloodGroup = user.BloodGroup;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
